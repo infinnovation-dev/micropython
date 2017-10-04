0bff37e45c04029aebd0abca378aa2f345348fbd
Colin Hogben <colin@infinnovation.co.uk>
2017-10-04 23:07:19 +0100

    mcux: Trivial machine module

---
+++ ports/mcux/modmachine.c
@@ -0,0 +1,29 @@
+#include "py/mpstate.h"
+#include "fsl_common.h"
+#include "fsl_clock.h"
+
+STATIC mp_obj_t machine_freq(void) {
+    //return mp_obj_new_int(CLOCK_GetCoreSysClkFreq());
+    return mp_obj_new_int(CLOCK_GetPlatClkFreq());
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(machine_freq_obj, machine_freq);
+
+STATIC const mp_rom_map_elem_t machine_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__),            MP_ROM_QSTR(MP_QSTR_machine) },
+
+    //{ MP_ROM_QSTR(MP_QSTR_reset),               MP_ROM_PTR(&machine_reset_obj) },
+#ifdef DEBUG
+    { MP_ROM_QSTR(MP_QSTR_info),                MP_ROM_PTR(&machine_info_obj) },
+#endif
+    { MP_ROM_QSTR(MP_QSTR_freq),                MP_ROM_PTR(&machine_freq_obj) },
+    //{ MP_ROM_QSTR(MP_QSTR_unique_id),           MP_ROM_PTR(&machine_unique_id_obj) },
+    //{ MP_ROM_QSTR(MP_QSTR_idle),                MP_ROM_PTR(&machine_idle_obj) },
+    //{ MP_ROM_QSTR(MP_QSTR_sleep),               MP_ROM_PTR(&machine_sleep_obj) },
+};
+
+STATIC MP_DEFINE_CONST_DICT(machine_module_globals, machine_module_globals_table);
+
+const mp_obj_module_t mp_module_machine = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t*)&machine_module_globals,
+};
--- ports/mcux/mpconfigport.h
+++ ports/mcux/mpconfigport.h
@@ -86,6 +86,11 @@ typedef long mp_off_t;
 #define MICROPY_HW_BOARD_NAME "FRDM-K64F"
 #define MICROPY_HW_MCU_NAME "ARM"
 
+extern const struct _mp_obj_module_t mp_module_machine;
+
+#define MICROPY_PORT_BUILTIN_MODULES \
+    { MP_ROM_QSTR(MP_QSTR_machine),     MP_ROM_PTR(&mp_module_machine) }, \
+
 #define MP_STATE_PORT MP_STATE_VM
 
 #define MICROPY_PORT_ROOT_POINTERS \
