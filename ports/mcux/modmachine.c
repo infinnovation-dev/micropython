d1979ad13dfd08ae57532fbef4fe5089a93f2579
Colin Hogben <colin@infinnovation.co.uk>
2017-10-09 22:17:23 +0100

    mcux/modmachine: Add reset() and unique_id()

--- ports/mcux/modmachine.c
+++ ports/mcux/modmachine.c
@@ -3,12 +3,29 @@
 
 #include "fsl_common.h"
 #include "fsl_clock.h"
+#include "fsl_sim.h"
 
+// machine.freq()
 STATIC mp_obj_t machine_freq(void) {
     //return mp_obj_new_int(CLOCK_GetCoreSysClkFreq());
     return mp_obj_new_int(CLOCK_GetPlatClkFreq());
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(machine_freq_obj, machine_freq);
+
+// machine.reset()
+STATIC NORETURN mp_obj_t machine_reset(void) {
+    NVIC_SystemReset();
+    for (;;);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(machine_reset_obj, machine_reset);
+
+// machine.unique_id()
+STATIC mp_obj_t machine_unique_id(void) {
+    sim_uid_t uid;
+    SIM_GetUniqueId(&uid);
+    return mp_obj_new_bytes((byte *)&uid, sizeof(uid));
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(machine_unique_id_obj, machine_unique_id);
 
 STATIC const mp_rom_map_elem_t machine_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__),            MP_ROM_QSTR(MP_QSTR_machine) },
@@ -17,12 +34,12 @@ STATIC const mp_rom_map_elem_t machine_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_mem16), MP_ROM_PTR(&machine_mem16_obj) },
     { MP_ROM_QSTR(MP_QSTR_mem32), MP_ROM_PTR(&machine_mem32_obj) },
 
-    //{ MP_ROM_QSTR(MP_QSTR_reset),               MP_ROM_PTR(&machine_reset_obj) },
+    { MP_ROM_QSTR(MP_QSTR_reset),               MP_ROM_PTR(&machine_reset_obj) },
 #ifdef DEBUG
     { MP_ROM_QSTR(MP_QSTR_info),                MP_ROM_PTR(&machine_info_obj) },
 #endif
     { MP_ROM_QSTR(MP_QSTR_freq),                MP_ROM_PTR(&machine_freq_obj) },
-    //{ MP_ROM_QSTR(MP_QSTR_unique_id),           MP_ROM_PTR(&machine_unique_id_obj) },
+    { MP_ROM_QSTR(MP_QSTR_unique_id),           MP_ROM_PTR(&machine_unique_id_obj) },
     //{ MP_ROM_QSTR(MP_QSTR_idle),                MP_ROM_PTR(&machine_idle_obj) },
     //{ MP_ROM_QSTR(MP_QSTR_sleep),               MP_ROM_PTR(&machine_sleep_obj) },
 };
